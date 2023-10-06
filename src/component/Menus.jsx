872aef68e7f479f0ee2953c093ea7617f0c50c7b
Rajesh Jha <Rajesh.Jha@blueconchtech.com>
2023-10-06 19:56:20 +0530

    implemented the functionality

--- src/component/Menus.jsx
+++ src/component/Menus.jsx
@@ -1,27 +1,50 @@
-import { useState } from 'react';
-import Menu from './Menu';
+import { useState } from "react";
+import Menu from "./Menu";
 const Menus = ({ datas }) => {
   const [menus, setMenus] = useState(datas);
+
+  const handleBreakfast = () => {
+    const newMenu = datas.filter((data) => {
+      return data.category === "breakfast";
+    });
+    setMenus(newMenu);
+  };
+  const handleLunch = () => {
+    const newMenu = datas.filter((data) => {
+      return data.category === "lunch";
+    });
+    setMenus(newMenu);
+  };
+  const handleShakes = () => {
+    const newMenu = datas.filter((data) => {
+      return data.category === "shakes";
+    });
+    setMenus(newMenu);
+  };
+  const handleAll = () => {
+    setMenus(datas);
+  };
+
   console.log(datas);
   return (
-    <main className='menu-container'>
+    <main className="menu-container">
       {/* Title container */}
-      <section className='title-container'>
-        <h1 className='text-5xl mb-[10px]'>Our Menu</h1>
+      <section className="title-container">
+        <h1 className="text-5xl mb-[10px]">Our Menu</h1>
         <div></div>
       </section>
       {/* button container */}
-      <section className='btn-container'>
-        <button>All</button>
-        <button>Breakfast</button>
-        <button>Lunch</button>
-        <button>Shakes</button>
+      <section className="btn-container">
+        <button onClick={handleAll}>All</button>
+        <button onClick={handleBreakfast}>Breakfast</button>
+        <button onClick={handleLunch}>Lunch</button>
+        <button onClick={handleShakes}>Shakes</button>
       </section>
       {/* Menu section */}
-      <section className='data-layout'>
+      <section className="data-layout">
         {menus.map((data) => {
           return (
-            <section className='data-container'>
+            <section className="data-container">
               <Menu {...data} key={data.id} />
             </section>
           );
